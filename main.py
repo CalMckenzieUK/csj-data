05c488a858d7deae3be6391d4b4987e8d9e6b5cf
Cal Mckenzie <87912885+CalMckenzieUK@users.noreply.github.com>
2023-11-02 01:09:01 +0000

    added some styling and fed variables into index.html

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def main():
-    message = 'lol'
-    return flask.render_template('index.html', message)
+    hello = 'Hello World!'
+    main_content_title = 'main content title'
+    main_content = 'main content'
+    footer = 'footer'
+    side_title = 'side title'
+    return flask.render_template('index.html', yo=hello, side_title=side_title, main_content_title=main_content_title, main_content=main_content, footer=footer)
 
-
-def hello():
-    return 'Hello, World!'
 app.run(debug=True, host='0.0.0.0', port=5000)
 main()
 
