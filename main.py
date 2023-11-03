7a21fcdd1e473ba66e2ce1d33dc8c5aeac0de29c
Cal Mckenzie <87912885+CalMckenzieUK@users.noreply.github.com>
2023-11-03 22:48:15 +0000

    adjusted ccs to make main div scrollable

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def main():
     homepage_title = "Civil Service Jobs"
     main_content_title = 'Current vacancies'
     main_content = 'main content'
-    footer = 'footer'
-    side_title = 'side title'
+    footer = 'Mckenzie'
+    side_title = 'Options'
     return flask.render_template('index.html',  tables=[ads.to_html(classes='data', index=False)], titles=ads.columns.values, title=homepage_title, side_title=side_title, main_content_title=main_content_title, main_content=main_content, footer=footer)
 
 app.run(debug=True, host='0.0.0.0', port=5000)
