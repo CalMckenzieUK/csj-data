d6986ff6d968f90adad4b6ea9c9eb49987560df4
Cal Mckenzie <87912885+CalMckenzieUK@users.noreply.github.com>
2023-11-05 19:41:11 +0000

    attempted to add sid scraping

--- csj_scrape.py
+++ csj_scrape.py
@@ -1,11 +1,18 @@
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
 def scrape():
 # Send GET request to website URL
-    url = 'https://www.civilservicejobs.service.gov.uk/csr/index.cgi?SID=cGFnZWNsYXNzPVNlYXJjaCZwYWdlYWN0aW9uPXNlYXJjaGNvbnRleHQmb3duZXI9NTA3MDAwMCZvd25lcnR5cGU9ZmFpciZjb250ZXh0aWQ9NTY1MDIwNzEmcmVxc2lnPTE2OTkwMjA0MjEtODA5ZWEzOTAzZGYyY2RmZWZlMDA4NDQyNjg5MzYyOWZiYmRkNWJjYQ=='
+    
+    csj_url = 'https://www.civilservicejobs.service.gov.uk/csr/index.cgi'
+    csj_response = requests.get(csj_url)
+    csj_soup = BeautifulSoup(csj_response.content, 'html.parser')
+    csj_job_postings = csj_soup.find_all('form')
+    narrowed = csj_job_postings[1]
+    sid = narrowed.find('input', {'name': 'return_sid'}).get('value')
+    url = 'https://www.civilservicejobs.service.gov.uk/csr/index.cgi?SID=cGFnZWNsYXNzPVNlYXJjaCZvd25lcj01MDcwMDAwJm93bmVydHlwZT1mYWlyJmNvbnRleHRpZD01NjY3NjAzMCZwYWdlYWN0aW9uPXNlYXJjaGNvbnRleHQmcmVxc2lnPTE2OTkyMTI1NTctNjUzMDUzNDE2NzcyMzA1YWIzYjZhMzEzMmY4YzNhMjFmMzQxYTk3ZA=='
+#    url = f'https://www.civilservicejobs.service.gov.uk/csr/index.cgi?SID={str(sid)}'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     job_postings = soup.find_all('li', class_='search-results-job-box')
@@ -21,9 +28,10 @@ def scrape():
         job_data.append([title, department, location, salary, closing_date, uid])
     
     df = pd.DataFrame(job_data, columns=['Title', 'Department', 'Location', 'Salary', 'Closing Date', 'UID'])
+    print(soup.prettify()) 
     return df
-        
-
+    
+scrape()
 
 
     
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import flask
+from csj_scrape import scrape
 app = flask.Flask(__name__)
-from csj_scrape import ads
+
 
 @app.route('/')
 def main():
+    ads = scrape()
     homepage_title = "Civil Service Jobs"
     main_content_title = 'Current vacancies'
     main_content = 'main content'
@@ -12,5 +14,6 @@ def main():
     return flask.render_template('index.html',  tables=[ads.to_html(classes='data', index=False)], titles=ads.columns.values, title=homepage_title, side_title=side_title, main_content_title=main_content_title, main_content=main_content, footer=footer)
 
 app.run(debug=True, host='0.0.0.0', port=5000)
-main()
 
+if __name__ == '__main__':
+    main()
