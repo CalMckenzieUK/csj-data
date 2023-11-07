353988939d3622f3c9bd2a01a21f8fcc10d56fd0
Cal Mckenzie <87912885+CalMckenzieUK@users.noreply.github.com>
2023-11-07 14:15:35 +0000

    dynamic link generated using search button

--- csj_scrape.py
+++ csj_scrape.py
@@ -1,39 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
 
-def scrape():
-# Send GET request to website URL
-    
-    csj_url = 'https://www.civilservicejobs.service.gov.uk/csr/index.cgi'
-    csj_response = requests.get(csj_url)
-    csj_soup = BeautifulSoup(csj_response.content, 'html.parser')
-    csj_job_postings = csj_soup.find_all('form')
-    narrowed = csj_job_postings[1]
-    sid = narrowed.find('input', {'name': 'return_sid'}).get('value')
-    url = 'https://www.civilservicejobs.service.gov.uk/csr/index.cgi?SID=cGFnZWNsYXNzPVNlYXJjaCZvd25lcj01MDcwMDAwJm93bmVydHlwZT1mYWlyJmNvbnRleHRpZD01NjY3NjAzMCZwYWdlYWN0aW9uPXNlYXJjaGNvbnRleHQmcmVxc2lnPTE2OTkyMTI1NTctNjUzMDUzNDE2NzcyMzA1YWIzYjZhMzEzMmY4YzNhMjFmMzQxYTk3ZA=='
-#    url = f'https://www.civilservicejobs.service.gov.uk/csr/index.cgi?SID={str(sid)}'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+#function that goes to webpage, clicks a button called 'search', and returns the html of the page
+def button_click():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://www.civilservicejobs.service.gov.uk/csr/index.cgi')
+    driver.find_element(By.NAME, 'search_button').click()
+    #sleep(5)
+    all_results_pages = []
+    page = 1
+    html = driver.page_source
+    driver.quit()
+    return html
+
+def scrape(url):
+    soup = BeautifulSoup(url, 'html.parser')
     job_postings = soup.find_all('li', class_='search-results-job-box')
     job_data = []
     for posting in job_postings:
-        ad = posting.get_text().strip().splitlines()
-        title = ad[0]
-        department = ad[1]
-        location = ad[2]
-        salary = ad[4]
-        closing_date = ad[6]
-        uid = ad[7]
-        job_data.append([title, department, location, salary, closing_date, uid])
+        try: 
+            ad = posting.get_text().strip().splitlines()
+            ad = list(filter(None, ad))
+            print(ad)
+            title = ad[0]
+            department = ad[1]
+            location = ad[2]
+            salary = ad[3]
+            closing_date = ad[4]
+            uid = ad[5]
+            job_data.append([title, department, location, salary, closing_date, uid])
+        except:
+            pass
     
     df = pd.DataFrame(job_data, columns=['Title', 'Department', 'Location', 'Salary', 'Closing Date', 'UID'])
-    print(soup.prettify()) 
-    return df
+    print(df)
     
-scrape()
 
-
-    
+scrape(button_click())
 # Loop through each job posting and extract details
 ads = scrape()
