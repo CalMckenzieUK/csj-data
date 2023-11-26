6cb7e87f90b880a55a3178881fda9454a300a920
Cal Mckenzie <87912885+CalMckenzieUK@users.noreply.github.com>
2023-11-26 08:53:51 +0000

    further st changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st 
 import pandas as pd
 
-st.write(''' # first line
+st.write(''' HI
+         # first line
          # second line
          *third line* 
-         # fourth line''')
+         fourth line''')
 
