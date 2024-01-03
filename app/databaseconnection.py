2600b21c5651adb8832f015f3a72638e53891321
Cal Mckenzie <87912885+CalMckenzieUK@users.noreply.github.com>
2024-01-03 19:59:20 +0000

    db test

--- app/databaseconnection.py
+++ app/databaseconnection.py
@@ -6,25 +6,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def database_query(sql_query):
-    try:
-        connection = MySQLdb.connect(
-        host=os.getenv("DATABASE_HOST"),
-        user=os.getenv("DATABASE_USERNAME"),
-        passwd=os.getenv("DATABASE_PASSWORD"),
-        db=os.getenv("DATABASE"),
+    # try:
+    #     connection = MySQLdb.connect(
+    #     host=os.getenv("DATABASE_HOST"),
+    #     user=os.getenv("DATABASE_USERNAME"),
+    #     passwd=os.getenv("DATABASE_PASSWORD"),
+    #     db=os.getenv("DATABASE"),
+    #     autocommit=True,
+    #     # ssl_mode="VERIFY_iDENTITY",
+    #     ssl={"ca": "/etc/ssl/certs/ca-certificates.crt"})
+    # except:
+    connection = MySQLdb.connect(
+        host=os.environ["DATABASE_HOST"],
+        user=os.environ["DATABASE_USERNAME"],
+        passwd=os.environ["DATABASE_PASSWORD"],
+        db=os.environ["DATABASE"],
         autocommit=True,
         # ssl_mode="VERIFY_iDENTITY",
         ssl={"ca": "/etc/ssl/certs/ca-certificates.crt"})
-    except:
-        connection = MySQLdb.connect(
-        host=os.environ("DATABASE_HOST"),
-        user=os.environ("DATABASE_USERNAME"),
-        passwd=os.environ("DATABASE_PASSWORD"),
-        db=os.environ("DATABASE"),
-        autocommit=True,
-        # ssl_mode="VERIFY_iDENTITY",
-        ssl={"ca": "/etc/ssl/certs/ca-certificates.crt"})
-
     try:
         c = connection.cursor()
         c.execute(sql_query)
@@ -39,4 +38,4 @@ def database_query(sql_query):
 if __name__ == '__main__':
     with open('app/SQL/create_ad_qualities.sql', 'r') as sql_file:
         sql_query = sql_file.read()
-    print(database_query(sql_query))
+    print(database_query('select * from all_time_listings limit 5'))
