b07c847fe18d4ac1a1970cf0cb23c106f44d6c8f
Cal Mckenzie <87912885+CalMckenzieUK@users.noreply.github.com>
2024-01-05 17:05:32 +0000

    db conn

--- app/databaseconnection.py
+++ app/databaseconnection.py
@@ -6,17 +6,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def database_query(sql_query):
-    # try:
-    #     connection = MySQLdb.connect(
-    #     host=os.getenv("DATABASE_HOST"),
-    #     user=os.getenv("DATABASE_USERNAME"),
-    #     passwd=os.getenv("DATABASE_PASSWORD"),
-    #     db=os.getenv("DATABASE"),
-    #     autocommit=True,
-    #     # ssl_mode="VERIFY_iDENTITY",
-    #     ssl={"ca": "/etc/ssl/certs/ca-certificates.crt"})
-    # except:
-    connection = MySQLdb.connect(
+    try:
+        connection = MySQLdb.connect(
+        host=os.getenv("DATABASE_HOST"),
+        user=os.getenv("DATABASE_USERNAME"),
+        passwd=os.getenv("DATABASE_PASSWORD"),
+        db=os.getenv("DATABASE"),
+        autocommit=True,
+        # ssl_mode="VERIFY_iDENTITY",
+        ssl={"ca": "/etc/ssl/certs/ca-certificates.crt"})
+    except:
+        connection = MySQLdb.connect(
         host=os.environ["DATABASE_HOST"],
         user=os.environ["DATABASE_USERNAME"],
         passwd=os.environ["DATABASE_PASSWORD"],
